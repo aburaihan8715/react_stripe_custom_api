ee975a68212d2158ab8b523bd6315decdbfd4d12
aburahan8721 <aburaihan8721@gmail.com>
2024-01-21 17:36:14 +0600

    add db

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import { app } from "./app.js";
 import { serverPort } from "./src/config/constant.js";
+import { connectDb } from "./src/config/db.js";
 
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:${serverPort}`);
+  await connectDb();
 });
